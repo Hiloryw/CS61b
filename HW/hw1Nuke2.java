1faeed0775c22ea56dda5b90b03482426473a99b
Hiloryw <36879466+Hiloryw@users.noreply.github.com>
2018-07-06 15:34:57 +0800

    Add files via upload

---
+++ HW/hw1Nuke2.java
@@ -0,0 +1,37 @@
+/* OpenCommercial.java */
+
+import java.net.*;
+import java.io.*;
+
+/**  A class that provides a main function to read five lines of a commercial
+ *   Web page and print them in reverse order, given the name of a company.
+ */
+
+class Nuke2 {
+
+  /** Prompts the user for the name X of a company (a single string), opens
+   *  the Web site corresponding to www.X.com, and prints the first five lines
+   *  of the Web page in reverse order.
+   *  @param arg is not used.
+   *  @exception Exception thrown if there are any problems parsing the 
+   *             user's input or opening the connection.
+   */
+  public static void main(String[] arg) throws Exception {
+
+    BufferedReader keyboard;
+    String inputLine;
+
+    keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+          /* Make sure the line is printed immediately. */
+    inputLine = keyboard.readLine();
+
+    /* Replace this comment with your solution.  */
+    String outLine;
+    outLine = inputLine.substring(0,1)+inputLine.substring(2);
+    
+    System.out.println(outLine);
+
+
+  }
+}
